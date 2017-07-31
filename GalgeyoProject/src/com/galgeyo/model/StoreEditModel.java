cb8e124537a37bf74e469ff3f45247cb7c594e3b
anjiyun <30362927+anjiyun@users.noreply.github.com>
2017-07-31 09:44:35 +0900

    Update StoreEditModel.java

--- GalgeyoProject/src/com/galgeyo/model/StoreEditModel.java
+++ GalgeyoProject/src/com/galgeyo/model/StoreEditModel.java
@@ -2,4 +2,7 @@ package com.galgeyo.model;
 
 public class StoreEditModel {
 
+  //입력받은 데이터로 매장정보를 수정
+  public void updateStoreInfo(String storeName, String ownNo, String tel, String addr, String type, String pwd){}
+  
 }
