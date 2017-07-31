cd3fd80065547bc0d8169f343916cd07b214e348
anjiyun <30362927+anjiyun@users.noreply.github.com>
2017-07-31 09:53:55 +0900

    Update UserMainModel.java

--- GalgeyoProject/src/com/galgeyo/model/UserMainModel.java
+++ GalgeyoProject/src/com/galgeyo/model/UserMainModel.java
@@ -3,19 +3,18 @@ package com.galgeyo.model;
 public class UserMainModel {
   
   //즐겨찾기 데이터 가져오기
-  public void getFavorite(){}
-    
-
+  public HashMap getFavorite(){}
+   
   //최근주문내역 데이터 가져오기
-    public void getRecentlyOrder(){}
+    public HashMap getRecentlyOrder(){}
   
   //삭제 후 데이터 업데이트(즐겨찾기)
-     public void updateFavoriteData(String userId){}
+     public HashMap updateFavoriteData(String userId){}
   
   //삭제 후 데이터 업데이트(최근주문내역)
-    public void updateRecentlyData(String userId){}
+    public HashMap updateRecentlyData(String userId){}
   
   //주문한 메뉴 데이터 가져오기
-    public void getOrderData(String orderNo){}
+    public Order getOrderData(String orderNo){}
     
 }
