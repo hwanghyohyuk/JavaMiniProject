1251d1c149d8b4b57683ec005c7132cdb71b41e7
anjiyun <30362927+anjiyun@users.noreply.github.com>
2017-07-31 09:53:10 +0900

    Update MenuManagememtModel.java

--- GalgeyoProject/src/com/galgeyo/model/MenuManagememtModel.java
+++ GalgeyoProject/src/com/galgeyo/model/MenuManagememtModel.java
@@ -2,4 +2,17 @@ package com.galgeyo.model;
 
 public class MenuManagememtModel {
 
+  //매장의 메뉴 데이터 받아오기
+  public HashMap getMenu(String tel){}
+  
+  //검색한 키워드로 메뉴 데이터 받아오기
+  public HashMap selectMenu(String keyword){}
+  
+  //체크한 메뉴 삭제 후 업데이트
+  public HashMap updateMenuDelete(String[] menuNo){}
+  
+  //입력한 정보로 메뉴를 추가하고 업데이트
+  public HashMap updateMenuInsert(String MenuNo, String MenuName, String category,
+                                  int price, double discount, String imgURL, boolean orderYN){}
+
 }
