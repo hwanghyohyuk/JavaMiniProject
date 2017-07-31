d7fb158d4f203d708cc8e91132e61e7ac2bed440
anjiyun <30362927+anjiyun@users.noreply.github.com>
2017-07-31 09:30:05 +0900

    Update OrderMenuModel.java

--- GalgeyoProject/src/com/galgeyo/model/OrderMenuModel.java
+++ GalgeyoProject/src/com/galgeyo/model/OrderMenuModel.java
@@ -1,5 +1,17 @@
 package com.galgeyo.model;
 
 public class OrderMenuModel {
+ 
+  //매장 리스트 데이터 받아오기
+  public HashMap getStore(String category){}
+  
+  //해당 매장의 메뉴 데이터 받아오기
+  public HashMap getMenu(String tel){}
+  
+  //검색한 키워드로 매장 데이터 받아오기
+  public HashMap selectStore(String keyword){}
+  
+  //주문 내역 저장
+  public void sendOrder(Order o){}
 
 }
