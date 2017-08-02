8f7feea27426c74bfbf79f32a04cbd7bf9ea806b
anjiyun <30362927+anjiyun@users.noreply.github.com>
2017-08-02 10:14:56 +0900

    1

--- GalgeyoProject/src/com/galgeyo/controller/SessionController.java
+++ GalgeyoProject/src/com/galgeyo/controller/SessionController.java
@@ -13,15 +13,9 @@ public class SessionController {
 	// 패킷은 boolean,byte,Date,String 순서로 객체화된다
 	// 패킷에 필요한 정보를 담고 객체스트림으로 입출력한다.
 	
-<<<<<<< HEAD
-	//세션.프로퍼티스 파일에서 불러오는 메소드
+
 	public Object sessionLoad(){
-=======
-	public Object sessionLoad(){//세션.프로퍼티스 파일에서 불러오는 메소드
-<<<<<<< HEAD
->>>>>>> a919f37adf48453acc7684fa6e25cb9638147c47
-=======
->>>>>>> a919f37adf48453acc7684fa6e25cb9638147c47
+
 		Properties prop = new Properties();
 		try {
 			prop.load(new FileInputStream("session.properties"));
@@ -30,32 +24,14 @@ public class SessionController {
 		}
 		String session = prop.getProperty("session");
 		String[] split = session.split(",");
-<<<<<<< HEAD
-<<<<<<< HEAD
 		
 		if(split[0].equals("true")){	//true면 사용자
 			User user = new User(true, split[1], split[2], split[3], split[4]);
 			Object temp=user;			
 			return temp;
 		}else{							//false면 관리자
-=======
-=======
->>>>>>> a919f37adf48453acc7684fa6e25cb9638147c47
-		if(split[0].equals("true")){//true면 사용자
-			User user = new User(true, split[1], split[2], split[3], split[4]);
-			Object temp=user;			
-			return temp;
-		}else{//false면 관리자
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> a919f37adf48453acc7684fa6e25cb9638147c47
-=======
->>>>>>> a919f37adf48453acc7684fa6e25cb9638147c47
-			Manager manager= new Manager(false, split[1], split[2], split[3], split[4], split[5], split[6], split[7]);
-=======
 			System.out.println("false");
 			Manager manager= new Manager(false, split[1], split[2], split[3], split[4], split[5], split[6], split[7],Boolean.getBoolean(split[8]));
->>>>>>> HH
 			Object temp=manager;
 			return temp;
 		}		
