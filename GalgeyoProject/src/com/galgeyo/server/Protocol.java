78e242b7eb00210d360002959ebbdd647c18a9a4
anjiyun <30362927+anjiyun@users.noreply.github.com>
2017-08-02 11:11:40 +0900

    Merge branch 'HH' into JY

--- GalgeyoProject/src/com/galgeyo/server/Protocol.java
+++ GalgeyoProject/src/com/galgeyo/server/Protocol.java
@@ -4,15 +4,21 @@ public interface Protocol {
 	public static final boolean POST = true;
 	public static final boolean GET = false;
 <<<<<<< HEAD
+<<<<<<< HEAD
 	public static final byte LOGIN = 10;
 	public static final byte REG_USER = 20;
 	public static final byte REG_ID_CHECK = 21;
 	public static final byte REG_MOD_USER = 22;
 =======
+=======
+>>>>>>> HH
 	public static final byte LOGIN = 3;
 	public static final byte REG_USER = 4;
 	public static final byte REG_ID_CHECK = 5;
 	public static final byte REG_MOD_USER = 6;
+<<<<<<< HEAD
+>>>>>>> HH
+=======
 >>>>>>> HH
 	public static final byte FIND_STORE = 30;
 	public static final byte FIND_STORE_KR = 31;
