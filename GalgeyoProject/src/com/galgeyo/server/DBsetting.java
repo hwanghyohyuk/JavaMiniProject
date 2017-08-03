44917036d14e4361e1699c0c0330309fe71ba347
hwanghyohyuk <hhhwang618@naver.com>
2017-08-04 01:55:14 +0900

    11

--- GalgeyoProject/src/com/galgeyo/server/DBsetting.java
+++ GalgeyoProject/src/com/galgeyo/server/DBsetting.java
@@ -7,6 +7,6 @@ public interface DBsetting {
 	public static final String WAITING_LIST = "serverDB/waitinglist/";
 	public static final String BOOKING_LIST = "serverDB/bookinglist/";
 	public static final String FAVORITE_LIST = "serverDB/favoritelist/";	
-	public static final String IP = "192.168.40.170";
+	public static final String IP = "192.168.25.9";
 	
 }
