b2efcb7a02ece53749b61d9ec79077b836a2334d
HongMyungTeak <30407948+HongMyungTeak@users.noreply.github.com>
2017-08-03 15:07:25 +0900

    11

--- GalgeyoProject/src/com/galgeyo/vo/Arraylist.java
+++ GalgeyoProject/src/com/galgeyo/vo/Arraylist.java
@@ -1,8 +1,9 @@
 package com.galgeyo.vo;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Arraylist {
+public class Arraylist implements Serializable {
 private ArrayList<Menu> arrayList;
 
 public Arraylist(ArrayList<Menu> menulist) {
